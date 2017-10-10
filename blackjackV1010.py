b55d7b351e24a0c7178fbe02483a581654f034fd
Raphpat <rpattiecsi@gmail.com>
2017-10-10 16:31:00 +0200

    Update blackjackV1010.py

--- blackjackV1010.py
+++ blackjackV1010.py
@@ -12,7 +12,7 @@ def lancé(nb):
     des = [0 for i in range(nb)]
     for i in range(nb):
         des[i] = randint(1,6)
-    print(des)
+    #print(des)
     total = sum(des) #méthode sum() trouvée en ligne
     return(total)
 
@@ -21,6 +21,7 @@ def retourScores(j1, j2):
     print("Score banque = ", j2)
 
 #Premières comparaisons, pour voir si la partie n'est pas finie d'office
+#(quelqu'un au dessus de 21)
 def comparaisons1(j1, j2):
     if j1 > 21 and j2 > 21:
         retourScores(j1, j2)
@@ -35,24 +36,31 @@ def comparaisons1(j1, j2):
         retourScores(j1, j2)
         print("Vous avez gagné, la banque a perdu")
         return True
-    
-    
+    else:
+        return False
 
+#Comparaisons entre j1 et j2 pr déterminer vainqueur
 def comparaisons2(j1, j2):
-    #Comparaisons entre j1 et j2 pr déterminer vainqueur
     return 0
 
+#Début, lancé de tout les dés, sortie du scoreJoueur
 x = int(input('Nombre de dés lancés: '))
 scoreJoueur = lancé(x)
-    #print("Score joueur = ", scoreJoueur)
+print("Votre score: ", scoreJoueur)
 scoreBanque = lancé(x)
-    #print(scoreBanque)
+#print(scoreBanque)
 end = comparaisons1(scoreJoueur, scoreBanque)
-relance = str(input("Voulez vous relancer? (y/n): "))
-while relance == 'y':
-    x = int(input('Nombre de dés lancés: '))
-    scoreJoueur = lancé(x)
-    end = comparaisons1(scoreJoueur, scoreBanque)
-    if end == True:
-        break
+
+if end == False:
     relance = str(input("Voulez vous relancer? (y/n): "))
+    while relance == 'y':
+        x = int(input('Nombre de dés lancés: '))
+        scoreJoueur = lancé(x)
+        print("Votre score: ", scoreJoueur)
+        end = comparaisons1(scoreJoueur, scoreBanque)
+        if end == True:
+            break
+        relance = str(input("Voulez vous relancer? (y/n): "))
+else:
+    print("Fin de la partie")
+    #Ajouter impression compteur
